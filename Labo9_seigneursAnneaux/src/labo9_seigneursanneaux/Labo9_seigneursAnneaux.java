40e8c45c42164ecbec033c8c498af4179b77980c
Unknown <james.smith@heig-vd.ch>
2017-12-14 10:36:28 +0100

    add main

--- Labo9_seigneursAnneaux/src/labo9_seigneursanneaux/Labo9_seigneursAnneaux.java
+++ Labo9_seigneursAnneaux/src/labo9_seigneursanneaux/Labo9_seigneursAnneaux.java
@@ -11,11 +11,30 @@ package labo9_seigneursanneaux;
  */
 public class Labo9_seigneursAnneaux {
 
+    Labo9_seigneursAnneaux(){
+        Personne frodo = new Personne("Frodo", Lieu.Comte);
+        Ennemi sauron = new Ennemi("Sauron", Lieu.Destin);
+        System.out.println("-1-");
+        Anneau anneau = sauron.anneau();
+        System.out.println("-2-");
+        anneau.utiliser();
+        System.out.println("-3-");
+        anneau.definirProprietaire(frodo);
+        System.out.println("-4-");
+        anneau.utiliser();
+        System.out.println("-5-");
+        anneau.detruire();
+        System.out.println("-6-");
+        frodo.deplacer(Lieu.Destin);
+        anneau.detruire();
+        System.out.println("-7-");
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Labo9_seigneursAnneaux();
+        System.gc();
     }
     
 }
